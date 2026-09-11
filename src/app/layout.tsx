16c47fe7b7cc65df99632c883e4d591b57e236a8
Mohammed Mazhar Khan <7724697+immazhark@users.noreply.github.com>
2026-09-11 06:49:23 +0530

    design: load Stories field journal styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./home-experience.css";
 import "./our-work.css";
 import "./initiative-experience.css";
 import "./impact.css";
+import "./stories.css";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { Analytics } from "@/components/analytics";
