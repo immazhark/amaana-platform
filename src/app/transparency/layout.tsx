d70792cefa65c82e8c0537cf22f9d79737a801fc
Mohammed Mazhar Khan <7724697+immazhark@users.noreply.github.com>
2026-09-12 04:32:43 +0530

    a11y: load trust contrast safeguards for transparency

--- src/app/transparency/layout.tsx
+++ src/app/transparency/layout.tsx
@@ -1,4 +1,5 @@
 import "../trust-experience.css";
+import "../trust-accessibility.css";
 import "./transparency-wow.css";
 
 export default function TransparencyLayout({ children }: Readonly<{ children: React.ReactNode }>) {
