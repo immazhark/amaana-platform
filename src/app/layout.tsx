9f72b8dd6c58c43bb6eac90ada54cd4ed6165815
Mohammed Mazhar Khan <7724697+immazhark@users.noreply.github.com>
2026-09-11 06:40:10 +0530

    design: load initiative detail styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./brand.css";
 import "./media.css";
 import "./home-experience.css";
 import "./our-work.css";
+import "./initiative-experience.css";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { Analytics } from "@/components/analytics";
