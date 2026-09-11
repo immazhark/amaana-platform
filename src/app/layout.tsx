c9e6106072a2079f7f526a4e72f53a42351ca4c7
Mohammed Mazhar Khan <7724697+immazhark@users.noreply.github.com>
2026-09-11 06:51:14 +0530

    design: load Faith & Reflections styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import "./our-work.css";
 import "./initiative-experience.css";
 import "./impact.css";
 import "./stories.css";
+import "./faith.css";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { Analytics } from "@/components/analytics";
