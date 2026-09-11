477bcb5bc628539bf8f7e91f50906e4e71ddcb3a
Mohammed Mazhar Khan <7724697+immazhark@users.noreply.github.com>
2026-09-11 20:51:18 +0530

    design: load medical assistance styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import "./qurbani.css";
 import "./taleem.css";
 import "./winter.css";
 import "./dates.css";
+import "./medical.css";
 import "./impact.css";
 import "./stories.css";
 import "./faith.css";
