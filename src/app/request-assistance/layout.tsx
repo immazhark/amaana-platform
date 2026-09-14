aa5a623828a25a5149b1d7f8768a1c3bc32f1795
Mohammed Mazhar Khan <7724697+immazhark@users.noreply.github.com>
2026-09-14 17:24:50 +0530

    design: load assistance wow layer

--- src/app/request-assistance/layout.tsx
+++ src/app/request-assistance/layout.tsx
@@ -1,4 +1,5 @@
 import "../forms-experience.css";
+import "../assistance-wow.css";
 
 export default function AssistanceLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
